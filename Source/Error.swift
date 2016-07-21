a5483f4c28407deb74705b47861f4572dbc2ba40
Jack <xuwenjiejack@gmail.com>
2016-07-21 19:24:29 +0800

    Add ErrorType
    But it also need to modify

---
+++ Source/Error.swift
@@ -0,0 +1,86 @@
+//
+//  Error.swift
+//  xScanner
+//
+//  Created by 许文杰 on 7/17/16.
+//  Copyright © 2016 许文杰. All rights reserved.
+//
+
+protocol xScannerError: CustomDebugStringConvertible, CustomStringConvertible {
+    
+}
+
+/// Common Error
+enum CommonError: String, CustomDebugStringConvertible, CustomStringConvertible {
+    case createSocketError
+    case settingSocketError
+
+    var debugDescription: String {
+        print(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
+        return result()
+    }
+
+    var description: String {
+        return result()
+    }
+
+    private func result() -> String {
+        switch self {
+        case createSocketError:
+            return createSocketError.rawValue
+        case settingSocketError:
+            return settingSocketError.rawValue
+        }
+    }
+}
+
+/// ICMP Error
+enum ICMPError: String, CustomDebugStringConvertible, CustomStringConvertible {
+    case sendError
+    case unPacketError
+    case receiveError
+
+    var debugDescription: String {
+        print(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
+        return result()
+    }
+
+    var description: String {
+        return result()
+    }
+
+    private func result() -> String {
+        switch self {
+        case sendError:
+            return sendError.rawValue
+        case unPacketError:
+            return unPacketError.rawValue
+        case .receiveError:
+            return receiveError.rawValue
+        }
+    }
+}
+
+/// UDP Error
+enum UDPError: String, CustomDebugStringConvertible, CustomStringConvertible {
+    case sendError
+    case receiveError
+
+    var debugDescription: String {
+        print(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
+        return result()
+    }
+
+    var description: String {
+        return result()
+    }
+
+    private func result() -> String {
+        switch self {
+        case sendError:
+            return sendError.rawValue
+        case .receiveError:
+            return receiveError.rawValue
+        }
+    }
+}

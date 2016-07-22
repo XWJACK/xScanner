2f2d6765580c4abe9d0fe4972c1ece801564a67f
Jack <xuwenjiejack@gmail.com>
2016-07-22 11:40:15 +0800

    Improved code
    Add new extension and typealias, Rename function and struct

--- Source/Base.swift
+++ Source/Base.swift
@@ -6,68 +6,86 @@
 //  Copyright © 2016 XWJACK. All rights reserved.
 //
 
-import Foundation
+//  The Base function in totle framework
+
+//  But if you want to using pointer in swift.
+//  Remenber to release by your self when you don't needed, because is unsafe in swift.
+
+typealias xIP = in_addr_t
+typealias xPort = UInt16
+typealias xTimeout = Int32
+typealias xPid = UInt16
+typealias xSize = UInt16
+typealias xSocket = Int32
+
+/// Do not using sizeof(timeval)
+let xTimeSize = UInt32(strideof(timeval))
+let xIPV4 = UInt8(AF_INET)
 
 /**
 *  The struct of IP Header
+    In addition to we can using #import "ip.h" in xScanner.h
+    This file is exist in:
+    /Applications/Xcode.app/Contents/Developer/Platforms/   iPhoneSimulator.platform (iPhoneOS.platform)
+    /Developer/SDKs/                                        iPhoneSimulator.sdk      (iPhoneOS.sdk)
+    /usr/include/netinet/ip.h
 */
-struct ip {
-    var versionAndHeaderLength:UInt8
-    var differentiatedServices:UInt8
-    var totalLength:UInt16
-    var identification:UInt16
-    var flagsAndFragmentOffset:UInt16
-    var timeToLive:UInt8
-    var ipProtocol:UInt8
-    var headerCheckSum:UInt16
-    var sourceAddress:UInt32
-    var destinationAddress:UInt32
+struct IP {
+    var versionAndHeaderLength: UInt8
+    var differentiatedServices: UInt8
+    var totalLength: UInt16
+    var identification: UInt16
+    var flagsAndFragmentOffset: UInt16
+    var timeToLive: UInt8
+    var ipProtocol: UInt8
+    var headerCheckSum: UInt16
+    var sourceAddress: UInt32
+    var destinationAddress: UInt32
 }
 
 /**
-calculate checksum
+calculate checksum to check pack is correct
 
 - parameter buffer: buffer
-- parameter size:   size
+- parameter size:   buffer size
 
-- returns: checksum
+- returns: checksum with Unsigned Int 16 bytes
 */
-func xCheckSum(buffer:UnsafeMutablePointer<Void>, var _ size:UInt16) -> UInt16 {
-    var checksum:UInt32 = 0
-    var buf = UnsafeMutablePointer<UInt16>(buffer)
-    
+func xCheckSum(buffer: UnsafeMutablePointer<Void>, var _ size: xSize) -> UInt16 {
+    var checkSum: UInt32 = 0
+    var buff = UnsafeMutablePointer<UInt16>(buffer)
+
     while size > 1 {
-        checksum += UInt32(buf.memory)
-        buf = buf.successor()
+        checkSum += UInt32(buff.memory)
+        buff = buff.successor()
         size -= UInt16(sizeof(UInt16))
     }
-    
-    if size == 1 {
-        checksum += UInt32(UnsafeMutablePointer<UInt16>(buf).memory)
-    }
-    checksum = (checksum >> 16) + (checksum & 0xFFFF)
-    checksum += checksum >> 16
-    return ~UInt16(checksum)
+
+    if size == 1 { checkSum += UInt32(buff.memory) }
+
+    checkSum = (checkSum >> 16) + (checkSum & 0xFFFF)
+    checkSum += checkSum >> 16
+    return ~UInt16(checkSum)
 }
 
 
 /**
 Calculate time subtract
 
-- parameter recvTimeStamp: receive time stamp
+- parameter receiveTimeStamp: receive time stamp
 - parameter sendTimeStamp: send time stamp
 
-- returns: microsencond
+- returns: time with number of microsencond
 */
-func xTimeSubtract(recvTimeStamp:UnsafeMutablePointer<timeval>, _ sendTimeStamp:UnsafeMutablePointer<timeval>) -> Double {
+func xTimeSubtract(receiveTimeStamp: UnsafeMutablePointer<timeval>, _ sendTimeStamp: UnsafeMutablePointer<timeval>) -> Double {
     //calculate seconds
-    var timevalSec = recvTimeStamp.memory.tv_sec - sendTimeStamp.memory.tv_sec
+    var timevalSec = receiveTimeStamp.memory.tv_sec - sendTimeStamp.memory.tv_sec
     //calculate microsends
-    var timevalUsec = recvTimeStamp.memory.tv_usec - sendTimeStamp.memory.tv_usec
-    
+    var timevalUsec = receiveTimeStamp.memory.tv_usec - sendTimeStamp.memory.tv_usec
+
     //if microsends less then zero
     if timevalUsec < 0 {
-        timevalSec--
+        timevalSec -= 1
         timevalUsec = -timevalUsec
     }
     return (Double(timevalSec) * 1000.0 + Double(timevalUsec)) / 1000.0
@@ -76,13 +94,46 @@ func xTimeSubtract(recvTimeStamp:UnsafeMutablePointer<timeval>, _ sendTimeStamp:
 /**
 setting ip Address
 
-- parameter ipAddr:  ip Address
+- parameter ipAddress:  ip Address
 - parameter port:    destination port
-- parameter dstAddr: destination ip Address
+- parameter destinationAddress: destination ip Address
 */
-func xSettingIp(ipAddr:in_addr_t, _ port:UInt16 , _ dstAddr:UnsafeMutablePointer<sockaddr_in>) {
-    bzero(dstAddr, sizeof(sockaddr_in));/* do not use sizeof(dstAddr) */
-    dstAddr.memory.sin_family = UInt8(AF_INET);
-    dstAddr.memory.sin_port = port.bigEndian
-    dstAddr.memory.sin_addr.s_addr = ipAddr
+func xSettingIp(ipAddress: xIP, _ port: xPort, _ destinationAddress: UnsafeMutablePointer<sockaddr_in>) {
+    bzero(destinationAddress, sizeof(sockaddr_in));/* do not use sizeof(dstAddr) */
+    destinationAddress.memory.sin_family = xIPV4
+    destinationAddress.memory.sin_port = port.bigEndian
+    destinationAddress.memory.sin_addr.s_addr = ipAddress
+}
+
+/**
+ Get Process ID
+ 
+ - returns: xPid
+ */
+func xGetPid() -> xPid {
+    return UInt16(getpid())
+}
+
+///**
+// Get Thread ID
+// 
+// - returns: xTid
+// */
+//func xGetTid() -> xTid {
+//    return
+//}
+
+// MARK: - UInt32 ip address to String ip address
+extension xIP {
+    func toString() -> String? {
+        return String.fromCString(inet_ntoa(in_addr(s_addr: self)))
+    }
+}
+
+// MARK: - String to UInt32
+extension String {
+    func toxIP() -> xIP? {
+        let ip = inet_addr(self)
+        return ip != INADDR_NONE ? ip : nil
+    }
 }

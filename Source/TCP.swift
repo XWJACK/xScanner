47c08b6013d60d35f0e5ff2267f3ed2b8535da2a
Jack <xuwenjiejack@gmail.com>
2016-07-22 11:33:45 +0800

    Rename

--- Source/TCP.swift
+++ Source/TCP.swift
@@ -6,16 +6,16 @@
 //  Copyright © 2016 XWJACK. All rights reserved.
 //
 
-import Foundation
 
-public func xConnectWithTCP(ipAddress:in_addr_t, _ port:UInt16/*, _ receiveTimeout:Int32*/) -> Bool {
+func xConnectWithTCP(ipAddress: xIP, _ port:UInt16/*, _ receiveTimeout:Int32*/) -> Bool {
     let socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
     guard socketfd != -1 else { return false }
     
+    // Useless in TCP
     //var sendTimeout = timeval(tv_sec: 2, tv_usec: 0)
     //var recvTimeout = timeval(tv_sec: Int(receiveTimeout / 1000), tv_usec: receiveTimeout % 1000)
-    //guard setsockopt(socketfd, SOL_SOCKET, SO_SNDTIMEO, &sendTimeout, UInt32(sizeof(timeval))) != -1 else { return false }
-    //guard setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO, &recvTimeout, UInt32(sizeof(timeval))) != -1 else { return false }
+    //guard setsockopt(socketfd, SOL_SOCKET, SO_SNDTIMEO, &sendTimeout, xTimeSize) != -1 else { return false }
+    //guard setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO, &recvTimeout, xTimeSize) != -1 else { return false }
     
     var destinationIpAddress = sockaddr_in()
     xSettingIp(ipAddress, port, &destinationIpAddress)
@@ -23,10 +23,7 @@ public func xConnectWithTCP(ipAddress:in_addr_t, _ port:UInt16/*, _ receiveTimeo
         return unsafeBitCast(temp, UnsafePointer<sockaddr>.self)
     }
 
-    guard connect(socketfd, destinationIpAddr, UInt32(sizeof(sockaddr))) != -1 else {
-        //print(errno)
-        return false
-    }
+    if connect(socketfd, destinationIpAddr, UInt32(sizeof(sockaddr))) == -1 { return false }
     close(socketfd)
     return true
 }

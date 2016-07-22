ab0f621efd942e7f9758a3b65afcf375b5796917
Jack <xuwenjiejack@gmail.com>
2016-07-22 11:51:43 +0800

    Modify Calculate Class
    Remove init with String, allHostsIPWithString function, Because it
    useless in core.

--- Source/Calculate.swift
+++ Source/Calculate.swift
@@ -9,54 +9,61 @@
 import Foundation
 
 /// Calculate Network Information about all the possible IP address.....
-public class Calculate{
+class Calculate {
     
-    private var mask:in_addr_t
-    private var ip:in_addr_t
-    /**
-    if ip or netmask is error,return nil
+    private var mask: xIP
+    private var ip: xIP
     
-    - parameter ipAddress: ip
-    - parameter netmask:   mask
-    
-    - returns: return nil if error
-    */
-    init?(ipAddress:String, netmask:String){
-        ip = inet_addr(ipAddress)
-        mask = inet_addr(netmask)
-        
-        guard ip != INADDR_NONE else { return nil }
-        guard mask != INADDR_NONE else { return nil }
-    }
-    
-    init(ipAddress:in_addr_t, netmask:in_addr_t) {
+    init(ipAddress: xIP, netmask: xIP) {
         self.ip = ipAddress
         self.mask = netmask
     }
     
     /// calculate host number in lan(contain 0 and 255)
-    public var HostNumber:Int { return Int(~mask.bigEndian) + 1 }
+    var HostsNumber: Int { return Int(~mask.bigEndian) + 1 }
     
-    /// calculate first ip address like 192.168.1.0
-    public var firstIPAddress:String { return String.fromCString(inet_ntoa(in_addr(s_addr: ip & mask)))! }
+    /// calculate first ip address
+    var firstIPAddress: xIP { return ip & mask }
     
-    /// calculate all host ip address like 192.168.1.1~192.168.1.254
-    public var allHostIPWithString:[String] {
-        var temp = [String]()
-        for i in 0..<HostNumber - 2 {
-            let tempip = CFSwapInt32(CFSwapInt32(inet_addr(firstIPAddress)) + UInt32(i + 1))
-            temp.append(String.fromCString(inet_ntoa(in_addr(s_addr: tempip)))!)
+    var allHostsIPWithInt: [xIP] {
+        var tempIP: [xIP] = []
+        tempIP.reserveCapacity(HostsNumber - 2)
+        
+        for i in 0..<HostsNumber - 2 {
+            tempIP.append(CFSwapInt32(CFSwapInt32(firstIPAddress) + UInt32(i + 1)))
         }
-        return temp
-    }
-    
-    public var allHostIPWithInt:[in_addr_t] {
-        var temp = [in_addr_t]()
-        for i in 0..<HostNumber - 2 {
-            let tempip = CFSwapInt32(CFSwapInt32(inet_addr(firstIPAddress)) + UInt32(i + 1))
-            temp.append(tempip)
-        }
-        return temp
+        return tempIP
     }
 }
 
+// MARK: - To String
+extension Calculate {
+//    /**
+//     if ip or netmask is error,return nil
+//     
+//     - parameter ipAddress: ip
+//     - parameter netmask:   mask
+//     
+//     - returns: return nil if error
+//     */
+//    init?(ipAddress: String, netmask: String) {
+//        ip = inet_addr(ipAddress)
+//        mask = inet_addr(netmask)
+//        
+//        if ip == INADDR_NONE { return nil }
+//        if mask == INADDR_NONE { return nil }
+//    }
+//    
+//    /// calculate first ip address like 192.168.1.0
+//    var firstIPAddress: String { return (ip & mask).toString()! }
+//    
+//    /// calculate all host ip address like 192.168.1.1~192.168.1.254
+//    var allHostIPWithString: [String] {
+//        var temp = [String]()
+//        for i in 0..<HostsNumber - 2 {
+//            let tempip = CFSwapInt32(CFSwapInt32(inet_addr(firstIPAddress)) + UInt32(i + 1))
+//            temp.append(tempip.toString()!)
+//        }
+//        return temp
+//    }
+}

18e6346914240c164215fd3a37dbce0a49621d93
Jack <xuwenjiejack@gmail.com>
2016-07-26 10:43:25 +0800

    Modify name and remove scanner port by string ip

--- Source/ScannerPorts.swift
+++ Source/ScannerPorts.swift
@@ -2,47 +2,47 @@
 //  ScannerPorts.swift
 //  xScanner
 //
-//  Created by XWJACK on 4/1/16.
+//  Created by Jack on 4/1/16.
 //  Copyright © 2016 XWJACK. All rights reserved.
 //
 
 import Foundation
 
-public class ScannerPorts {
-    private var ipAddress:in_addr_t
-    //private var port:[UInt16]
-    private var timeout:Int = 1
+class ScannerPorts {
+    private var ipAddress: xIP
     
-    init?(ipAddress:String) {
-        self.ipAddress = inet_addr(ipAddress)
-        guard self.ipAddress != INADDR_NONE else { return nil }
-    }
-    init(ipAddress:in_addr_t) {
+//    init?(ipAddress: String) {
+//        self.ipAddress = inet_addr(ipAddress)
+//        guard self.ipAddress != INADDR_NONE else { return nil }
+//    }
+    init(ipAddress: xIP) {
         self.ipAddress = ipAddress
     }
     
-    public func xScannerPortsWithTCP() -> [UInt16] {
+    func xScannerPortsWithTCP() -> [xPort] {
         
-        var portOpen = [UInt16]()
+        var portOpen: [xPort] = []
         
-        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-        let group = dispatch_group_create()
-        let currentSemphore = dispatch_semaphore_create(50)
-        
-        for port in 0...65535 {
-            dispatch_semaphore_wait(currentSemphore, DISPATCH_TIME_FOREVER)
-            dispatch_group_async(group, queue, { [unowned self] in
-                if xConnectWithTCP(self.ipAddress, UInt16(port)) == true { portOpen.append(UInt16(port)) }
-                dispatch_semaphore_signal(currentSemphore)
-            })
-        }
-        
-        dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
+//        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+//        let group = dispatch_group_create()
+//        let currentSemphore = dispatch_semaphore_create(50)
+//        
+//        for port in 0...65535 {
+//            dispatch_semaphore_wait(currentSemphore, DISPATCH_TIME_FOREVER)
+//            dispatch_group_async(group, queue, { [unowned self] in
+//                if xConnectWithTCP(self.ipAddress, UInt16(port)) == true { portOpen.append(UInt16(port)) }
+//                dispatch_semaphore_signal(currentSemphore)
+//            })
+//        }
+//        
+//        dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
         return portOpen
     }
-    public func xScannerPortsWithUDP() {
+    
+    func xScannerPortsWithUDP() {
         
     }
+    
     deinit {
         print("destory")
     }

40601d2e2c41e26dc9dd941b6f29e09cf6d51e50
Jack <xuwenjiejack@gmail.com>
2016-07-27 17:13:17 +0800

    TCP Tests

--- Tests/TCPTests.swift
+++ Tests/TCPTests.swift
@@ -2,7 +2,7 @@
 //  TCPTests.swift
 //  xScanner
 //
-//  Created by XWJACK on 4/11/16.
+//  Created by Jack on 4/11/16.
 //  Copyright © 2016 XWJACK. All rights reserved.
 //
 
@@ -12,14 +12,23 @@ import XCTest
 class TCPTests: BaseTests {
 
     func testConnectWithTCP() {
-        print(xConnectWithTCP(self.locateAddress[0], 80))
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-            print(xConnectWithTCP(self.locateAddress[0], 80))
+        if xConnectWithTCP(locateAddresses[0], 80) {
+            print("connect \(locateAddresses[0].toString()!) 80 port success")
+        } else {
+            print("connect \(locateAddresses[0].toString()!) 80 port fail")
+        }
+        if xConnectWithTCP(locateAddresses[0], 666) {
+            print("connect \(locateAddresses[0].toString()!) 666 port success")
+        } else {
+            print("connect \(locateAddresses[0].toString()!) 666 port fail")
         }
     }
+
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//            print(xConnectWithTCP(self.locateAddresses[0], 80))
+//        }
+//    }
 }

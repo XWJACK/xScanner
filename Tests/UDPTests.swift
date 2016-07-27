9b00ea674ad8cf5f815a118fe920e0f269a6acba
Jack <xuwenjiejack@gmail.com>
2016-07-27 17:13:07 +0800

    UDP Tests

--- Tests/UDPTests.swift
+++ Tests/UDPTests.swift
@@ -2,7 +2,7 @@
 //  UDPTests.swift
 //  xScanner
 //
-//  Created by XWJACK on 4/10/16.
+//  Created by Jack on 4/10/16.
 //  Copyright © 2016 XWJACK. All rights reserved.
 //
 
@@ -12,6 +12,6 @@ import XCTest
 class UDPTests: BaseTests {
 
     func testUDP() {
-        xSendUDP(locateAddress[1], 666)
+        xSendUDP(locateAddresses[0], 666)
     }
 }

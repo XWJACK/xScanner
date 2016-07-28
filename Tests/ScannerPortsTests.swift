ee35dbbfc5eb7321c8888bb706c2a1d919a4e3a7
Jack <xuwenjiejack@gmail.com>
2016-07-28 09:36:49 +0800

    Scanner Port Tests

--- Tests/ScannerPortsTests.swift
+++ Tests/ScannerPortsTests.swift
@@ -2,7 +2,7 @@
 //  ScannerPortsTests.swift
 //  xScanner
 //
-//  Created by XWJACK on 4/1/16.
+//  Created by Jack on 4/1/16.
 //  Copyright © 2016 XWJACK. All rights reserved.
 //
 
@@ -12,14 +12,14 @@ import XCTest
 class ScannerPortsTests: BaseTests {
 
     func testScannerPortsWithTCP() {
-        let scannerport = ScannerPorts(ipAddress: locateAddress[0])
+        let scannerport = ScannerPorts(ipAddress: locateAddresses[0])
         print(scannerport.xScannerPortsWithTCP())
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 }

e41949fbc9c65f8cdb7ad75dca473d4a6fb8619f
Jack <xuwenjiejack@gmail.com>
2016-10-18 16:25:49 +0800

    Rebuild Error

--- Source/Error.swift
+++ Source/Error.swift
@@ -6,101 +6,34 @@
 //  Copyright © 2016 XWJACK. All rights reserved.
 //
 
+enum Result<Type> {
+    case cussess(Type)
+    case failure(Error)
+}
+
 protocol xScannerError: CustomDebugStringConvertible, CustomStringConvertible {
 
 }
 
-/// Common Error
-enum CommonError: String, CustomDebugStringConvertible, CustomStringConvertible {
+/// Scanner Error
+enum ScannerError: CustomDebugStringConvertible, Error {
     case createSocketError
-    case settingSocketError
+    case sendError
+    case unpacketError
+    case receiveError
+    case connectError
+    case unKnowError
 
     var debugDescription: String {
-        print(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
-        return result()
-    }
-
-    var description: String {
+//        print(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
         return result()
     }
 
     private func result() -> String {
         switch self {
         case .createSocketError:
-            return CommonError.createSocketError.rawValue
-        case .settingSocketError:
-            return CommonError.settingSocketError.rawValue
-        }
-    }
-}
-
-/// ICMP Error
-enum ICMPError: String, CustomDebugStringConvertible, CustomStringConvertible {
-    case sendError
-    case unPacketError
-    case receiveError
-
-    var debugDescription: String {
-        print(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
-        return result()
-    }
-
-    var description: String {
-        return result()
-    }
-
-    private func result() -> String {
-        switch self {
-        case .sendError:
-            return ICMPError.sendError.rawValue
-        case .unPacketError:
-            return ICMPError.unPacketError.rawValue
-        case .receiveError:
-            return ICMPError.receiveError.rawValue
-        }
-    }
-}
-
-/// UDP Error
-enum UDPError: String, CustomDebugStringConvertible, CustomStringConvertible {
-    case sendError
-    case receiveError
-
-    var debugDescription: String {
-        print(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
-        return result()
-    }
-
-    var description: String {
-        return result()
-    }
-
-    private func result() -> String {
-        switch self {
-        case .sendError:
-            return UDPError.sendError.rawValue
-        case .receiveError:
-            return UDPError.receiveError.rawValue
-        }
-    }
-}
-
-enum TCPError: String, CustomStringConvertible, CustomDebugStringConvertible {
-    case connectError
-
-    var debugDescription: String {
-        print(NSError(domain: NSPOSIXErrorDomain, code: Int(errno), userInfo: nil))
-        return result()
-    }
-
-    var description: String {
-        return result()
-    }
-
-    private func result() -> String {
-        switch self {
-        case .connectError:
-            return TCPError.connectError.rawValue
+            return ""
+        default: return "unknow Error"
         }
     }
 }
